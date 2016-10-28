5c103f818cec8153c15017ca29812b6555dc55e4
Lorsen <omarfroa@gmail.com>
2016-10-28 00:31:03 -0500

    - Functionality Done, will make it prettier

--- BLEChat/ViewController.m
+++ BLEChat/ViewController.m
@@ -12,8 +12,11 @@
 @interface ViewController ()
 
 @property (weak, nonatomic) IBOutlet UILabel *shieldNameLabel;
+@property (weak, nonatomic) IBOutlet UIProgressView *progressBar;
 
 @property (weak, nonatomic) IBOutlet UILabel *ButtonLabel;
+@property (weak, nonatomic) IBOutlet UISlider *ledBrightness;
+@property (weak, nonatomic) IBOutlet UISwitch *ledToggle;
 
 @end
 
@@ -30,23 +33,15 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
-    
-    // CHANGE 1.a: change this as you no longer need to instantiate the BLE Object like this
-    // nor should this ViewController be the delegate
-//    bleShield = [[BLE alloc] init];
-//    [bleShield controlSetup];
-//    bleShield.delegate = self;
-    
-    //CHANGE 4: add subscription to notifications from the app delegate
-    //These selector functions should be created from the old BLEDelegate functions
-    // One example has already been completed for you on the receiving of data function
+    self.ledBrightness.enabled = NO;
+    [self.ledBrightness setContinuous:NO];
     [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(onBLEDidConnect:) name:kBleConnectNotification object:nil];
     [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(onBLEDidDisconnect:) name:kBleDisconnectNotification object:nil];
     [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(onBLEDidUpdateRSSI:) name:kBleRSSINotification object:nil];
     
     // this example function "onBLEDidReceiveData:" is done for you, see below
     [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector (onBLEDidReceiveData:) name:kBleReceivedDataNotification object:nil];
+    
 }
 
 //setup auto rotation in code
@@ -68,13 +63,7 @@ NSTimer *rssiTimer;
     self.labelRSSI.text = rssi.stringValue; // when RSSI read is complete, display it
 }
 
-// OLD FUNCTION: parse the received data using BLEDelegate protocol
--(void) bleDidReceiveData:(unsigned char *)data length:(int)length
-{
-    NSData *d = [NSData dataWithBytes:data length:length];
-    NSString *s = [[NSString alloc] initWithData:d encoding:NSUTF8StringEncoding];
-    self.label.text = s;
-}
+
 
 // NEW FUNCTION EXAMPLE: parse the received data from NSNotification
 -(void) onBLEDidReceiveData:(NSNotification *)notification
@@ -83,44 +72,16 @@ NSTimer *rssiTimer;
     NSString *s = [[NSString alloc] initWithData:d encoding:NSUTF8StringEncoding];
    
     dispatch_async(dispatch_get_main_queue(), ^(){
-        self.label.text = s;
-        
         NSLog(@"%@", s);
-        
-        if([s isEqualToString:@"0"]){
-            self.ButtonLabel.text= @"unpressed";
-        }
-        else{
-            self.ButtonLabel.text= @"pressed";
-        }
-
     });
+    
+    NSString *opCode = [s substringToIndex:1];
+    
+    if([opCode isEqualToString:@"1"]){
+        [self flex: [s substringFromIndex: 1]];
+    }
 }
 
-// we disconnected, stop running
-//- (void) bleDidDisconnect
-//{
-//    //CHANGE 5.b: remove all instances of the button at top
-//    [self.buttonConnect setTitle:@"Connect" forState:UIControlStateNormal];
-//    
-//    [rssiTimer invalidate];
-//}
-
-//CHANGE 7: create function called from "BLEDidConnect" notification (you can change the function below)
-// in this function, update a label on the UI to have the name of the active peripheral
-// you might be interested in the following method:
-// NSString *deviceName =[notification.userInfo objectForKey:@"deviceName"];
-// now just wait to send or receive
-
-//-(void) bleDidConnect
-//{
-//    //CHANGE 5.a: Remove all usage of the connect button and remove from storyboard
-//    [self.spinner stopAnimating];
-//    [self.buttonConnect setTitle:@"Disconnect" forState:UIControlStateNormal];
-//    
-//    // Schedule to read RSSI every 1 sec.
-//    rssiTimer = [NSTimer scheduledTimerWithTimeInterval:(float)1.0 target:self selector:@selector(readRSSITimer:) userInfo:nil repeats:YES];
-//}
 
 -(void) onBLEDidConnect:(NSNotification *)notification
 {
@@ -130,74 +91,62 @@ NSTimer *rssiTimer;
 
 -(void) onBLEDidDisconnect:(NSNotification *)notification
 {
+    [[self navigationController] popViewControllerAnimated:YES];
+}
+
+- (IBAction)brightnessChanged:(id)sender {
+    NSString* s = [NSString stringWithFormat:@"0%.0f\n", self.ledBrightness.value];
+    [self BLEShieldSend: s];
+}
+- (IBAction)ledToggle:(id)sender {
+    //Opcode for LED == 0
+    NSString* s;
+    if([self.ledToggle isOn]) {
+        s = @"0" @"on";
+        self.ledBrightness.enabled = YES;
+    }
+    else {
+        s = @"0" @"off";
+        self.ledBrightness.value = 180;
+        self.ledBrightness.enabled = NO;
+    }
+    [self BLEShieldSend:s];
 }
 
 
-
-
 #pragma mark - UI operations storyboard
-- (IBAction)BLEShieldSend:(id)sender
+- (void)BLEShieldSend:(NSString*) sendValue
 {
     
     //Note: this function only needs a name change, the BLE writing does not change
     NSString *s;
     NSData *d;
     
-    if (self.textField.text.length > 16)
-        s = [self.textField.text substringToIndex:16];
-    else
-        s = self.textField.text;
 
-    s = [NSString stringWithFormat:@"%@\r\n", s];
+    s = [NSString stringWithFormat:@"%@", sendValue ];
     d = [s dataUsingEncoding:NSUTF8StringEncoding];
     
     [self.bleShield write:d];
 }
 
+-(void)flex:(NSString*) flexValue
+{
+    if([flexValue isEqualToString:@"0"]){
+        NSLog(@"Flat");
+        self.ButtonLabel.text = @"Flat";    //TODO: use main queue
+    }
+    else if([flexValue isEqualToString:@"1"]){
+        NSLog(@"Up");
+        self.ButtonLabel.text = @"Up";
+        self.progressBar.progress = self.progressBar.progress+.1;
+        
+    }
+    else if([flexValue isEqualToString:@"2"]){
+        NSLog(@"Down");
+        self.ButtonLabel.text = @"Down";
+        self.progressBar.progress = self.progressBar.progress-.1;
+    }
+}
 
-//// CHANGE 1.b: change this as you no longer need to search for perpipherals in this view controller
-//- (IBAction)BLEShieldScan:(id)sender
-//{
-//    // disconnect from any peripherals
-//    if (bleShield.activePeripheral)
-//        if(bleShield.activePeripheral.state == CBPeripheralStateConnected)
-//        {
-//            [[bleShield CM] cancelPeripheralConnection:[bleShield activePeripheral]];
-//            return;
-//        }
-//    
-//    // set peripheral to nil
-//    if (bleShield.peripherals)
-//        bleShield.peripherals = nil;
-//    
-//    //start search for peripherals with a timeout of 3 seconds
-//    // this is an asynchronous call and will return before search is complete
-//    [bleShield findBLEPeripherals:3];
-//    
-//    // after three seconds, try to connect to first peripheral
-//    [NSTimer scheduledTimerWithTimeInterval:(float)3.0
-//                                     target:self
-//                                   selector:@selector(connectionTimer:)
-//                                   userInfo:nil
-//                                    repeats:NO];
-//    
-//    // give connection feedback to the user
-//    [self.spinner startAnimating];
-//}
-
-//// CHANGE 1.c: change this as you no longer need to create the connection in this view controller
-//// Called when scan period is over to connect to the first found peripheral
-//-(void) connectionTimer:(NSTimer *)timer
-//{
-//    if(bleShield.peripherals.count > 0)
-//    {
-//        // connect to the first found peripheral
-//        [bleShield connectPeripheral:[bleShield.peripherals objectAtIndex:0]];
-//    }
-//    else
-//    {
-//        [self.spinner stopAnimating];
-//    }
-//}
 
 @end
